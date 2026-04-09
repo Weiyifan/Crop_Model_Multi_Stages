0385b00f989f4a34d8af4a8c085e0dbaf742fca6
Wei Yifan <Weiyifan@users.noreply.github.com>
2026-04-09 13:58:44 +0800

    Initialize crop model multi stages module

---
+++ src/crop_model_multi_stages/__init__.py
@@ -0,0 +1,28 @@
+"""
+Crop_Model_Multi_Stages - 多物候期作物生长模拟模型
+
+该模型实现了：
+- 自定义多物候期配置
+- 积温(GDD)驱动的物候发育
+- Beer-Lambert光截获定律
+- 动态生物量分配
+- 环境胁迫响应
+"""
+
+__version__ = "1.0.0"
+__author__ = "Wei Yifan"
+__email__ = "silence.63@163.com"
+
+from .phenology import PhenologyStage, PhenologyConfig
+from .parameters import CropParameters
+from .core import CropModel
+from .weather import generate_weather, WeatherDataLoader
+
+__all__ = [
+    "PhenologyStage",
+    "PhenologyConfig",
+    "CropParameters",
+    "CropModel",
+    "generate_weather",
+    "WeatherDataLoader"
+]
